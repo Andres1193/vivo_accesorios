3cd715337ab7f831e6791e3be4188cc82bc04322
Kratoris <fegm8042@gmail.com>
2023-05-01 18:27:11 -0500

    cosas

--- src/app/usuario/components/registro/registro.component.ts
+++ src/app/usuario/components/registro/registro.component.ts
@@ -5,7 +5,8 @@ import { UsuarioService } from '../../services/usuario.service';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component00.html',
-  styleUrls: ['./registro.component00.css']
+  styleUrls: ['./registro.component00.css'],
+
 })
 export class RegistroComponent implements OnInit{
 
@@ -49,3 +50,33 @@ export class RegistroComponent implements OnInit{
 
 }
 
+$("main").addClass("pre-enter").removeClass("with-hover");
+setTimeout(function(){
+	$("main").addClass("on-enter");
+}, 500);
+setTimeout(function(){
+	$("main").removeClass("pre-enter on-enter");
+	setTimeout(function(){
+		$("main").addClass("with-hover");
+	}, 50);
+}, 3000);
+
+$("h1 a").click(function(){
+	$(this).siblings().removeClass("active");
+	$(this).addClass("active");
+	if ($(this).is("#link-signup")) {
+		$("#form-login").removeClass("active");
+		$("#intro-login").removeClass("active");
+		setTimeout(function(){
+			$("#form-signup").addClass("active");
+			$("#intro-signup").addClass("active");
+		}, 50);
+	} else {
+		$("#form-signup").removeClass("active");
+		$("#intro-signup").removeClass("active");
+		setTimeout(function(){
+			$("#form-login").addClass("active");
+			$("#intro-login").addClass("active");
+		}, 50);
+	}
+});
