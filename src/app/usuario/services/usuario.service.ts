9955c767534eca0a5aa9c7b3d91abbb6df09f7ea
Oscar Osorio <oscandosoval@gmail.com>
2023-05-01 22:44:03 -0500

    listado html css service

--- src/app/usuario/services/usuario.service.ts
+++ src/app/usuario/services/usuario.service.ts
@@ -18,5 +18,16 @@ export class UsuarioService {
        header.append('Access-Control-Allow-Methods','"POST"')
        header.append('Access-Control-Allow-Origin','http://localhost');
        return this.http.post(url,JSON.stringify(usuario),{headers:header});
- }
+  }
+
+ public getUsuarios(){
+  //defino la url donde esta el servicio
+  let  url ='http://localhost/tiendaservice/UsuariosService.php';
+     let header=new HttpHeaders();
+     header.append('Content-Type','aplication/json')
+     header.append('Access-Control-Allow-Methods','"GET"')
+     header.append('Access-Control-Allow-Origin','http://localhost');
+
+     return this.http.get<Usuario[]>(url,{headers:header});
+  }
 }
