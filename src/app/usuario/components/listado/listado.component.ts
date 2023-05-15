2842bd90ebf2f13ad33ebc65d88b2b55eb98ff55
Oscar Osorio <oscandosoval@gmail.com>
2023-05-14 22:06:12 -0500

    que miedo weon

--- src/app/usuario/components/listado/listado.component.ts
+++ src/app/usuario/components/listado/listado.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { Usuario } from '../../interfaces/usuario.interface';
 import { UsuarioService } from '../../services/usuario.service';
+declare var $: any;
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements AfterViewInit {
+
+  private self: ListadoComponent = this;
 
   public usuario: Usuario = {
     nombre: '',
@@ -23,6 +26,31 @@ export class ListadoComponent {
 
   constructor(public usuarioService: UsuarioService) {
 
+  }
+  ngAfterViewInit() {
+    $(document).ready(function() {
+      $('.js-check-all').on('click', function() {
+        if ( $(self).prop('checked') ) {
+          console.log("Hola mundo");
+          $('th input[type="checkbox"]').each(function() {
+            $(self).prop('checked', true);
+            $(self).closest('tr').addClass('active');
+          })
+        } else {
+          $('th input[type="checkbox"]').each(function() {
+            $(self).prop('checked', false);
+            $(self).closest('tr').removeClass('active');
+          })
+        }
+      });
+      $('th[scope="row"] input[type="checkbox"]').on('click', function() {
+        if ( $(self).closest('tr').hasClass('active') ) {
+          $(self).closest('tr').removeClass('active');
+        } else {
+          $(self).closest('tr').addClass('active');
+        }
+      });
+    });
   }
 
   ngOnInit(): void {
