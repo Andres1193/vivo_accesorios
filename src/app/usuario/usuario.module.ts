309d71eb02d1ff3cf79b428733851895ff7cf41b
Oscar Osorio <oscandosoval@gmail.com>
2023-05-30 21:56:00 -0500

    Se modifica el routing del login y otros componentes

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -4,11 +4,9 @@ import { RegistroComponent } from './components/registro/registro.component';
 import { ListadoComponent } from './components/listado/listado.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MainPageComponent } from './pages/main-page-usuario.component';
 import { EditarComponent } from './components/editar/editar.component';
 import { LoginComponent } from './components/login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
-import { AppRoutingModule } from '../app-routing.module';
 
 
 
@@ -16,19 +14,16 @@ import { AppRoutingModule } from '../app-routing.module';
   declarations: [
     RegistroComponent,
     ListadoComponent,
-    MainPageComponent,
     EditarComponent,
-    LoginComponent,
-    MainPageComponent
+    LoginComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    LoginRoutingModule
   ],
   exports: [
-    MainPageComponent
   ]
 })
 export class UsuarioModule { }
