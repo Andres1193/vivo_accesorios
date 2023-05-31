530c20c23cb09ce25875a8ecf9e757e2bfd5414c
kratoris <fegm8042@gmail.com>
2023-05-30 22:19:19 -0500

    fi x

--- src/app/usuario/login-routing.module.ts
+++ src/app/usuario/login-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from '../app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { ListadoComponent } from './components/listado/listado.component';
