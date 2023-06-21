303c75771377072f4a004e17f9e67116ba9a4b47
kratoris <fegm8042@gmail.com>
2023-06-20 23:03:28 -0500

    pedido fixed

--- src/app/pedido/interfaces/pedido.interface.ts
+++ src/app/pedido/interfaces/pedido.interface.ts
@@ -1,7 +1,6 @@
 import { Producto } from '../../producto/interfaces/producto.interface';
 export interface Pedido {
     codigoPedido: number;
-    codigoCliente: number;
     fecha: Date;
     nombreCliente: string;
     productos: Producto[];
