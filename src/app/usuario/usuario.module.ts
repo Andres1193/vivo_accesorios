23d233e157422ae547c5c17a910e2cc3b982cac6
kratoris <fegm8042@gmail.com>
2023-06-23 01:47:59 -0500

    Cambios de nombre modulo usuario

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegistroComponent } from './components/registro/registro.component';
 import { ListadoComponent } from '../listado-general/components/listado/listado.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { EditarComponent } from './components/editar/editar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from '../app-routing.module';
+import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
+import { EditarUsuarioComponent } from './components/editar-usuario/editar-usuario.component';
 
 
 
 @NgModule({
   declarations: [
-    RegistroComponent,
-    ListadoComponent,
-    EditarComponent
+    RegistroUsuarioComponent,
+    EditarUsuarioComponent
   ],
   imports: [
     CommonModule,
