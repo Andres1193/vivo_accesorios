b020c9b7992345f002f2aee43a6a535f00968612
mayle59 <mayrale2303@gmail.com>
2023-06-29 23:48:32 -0500

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,16 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { UsuarioModule } from "./usuario/usuario.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { LoginModule } from "./login/login.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ProductoModule } from "./producto/producto.module";
 import { RouterModule } from "@angular/router";
-
 import { ListadoGeneralModule } from "./listado-general/listado-general.module";
-
 import { RecetaModule } from "./receta/receta.module";
 import { ProveedorModule } from "./proveedor/proveedor.module";
 import { MateriasModule } from "./materias/materias.module";
 import { ClienteModule } from './cliente/cliente.module';
-import { ModalEliminarModule } from "./modal-eliminar/modal-eliminar.module";
 import { NgbModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { MaterialEmpaqueModule } from "./material-empaque/material-empaque.module";
-import { PedidoModule } from "./pedido/pedido.module";
 
 
 
@@ -38,8 +31,7 @@ import { PedidoModule } from "./pedido/pedido.module";
     RecetaModule,
     ProveedorModule,
     MateriasModule,
-    ClienteModule,
-    ModalEliminarModule
+    ClienteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
